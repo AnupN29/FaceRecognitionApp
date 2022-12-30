18b888ad970cb2db95481d64b7973cfe8cf6f34d
AnupN29 <nagdeveanup29@gmail.com>
2022-12-30 10:19:07 +0530

    New Version

--- src/functions.py
+++ src/functions.py
@@ -3,40 +3,65 @@ from mtcnn.mtcnn import MTCNN
 import numpy as np
 import pickle5 as pk
 from scipy.spatial import distance
- 
-  
-def extractFace(frames):
-    detector = MTCNN()
+from datetime import datetime
+
+def extractFace(frame):
+    detector = MTCNN(min_face_size = 120)
     faces = []
-    for i in range(len(frames)):
-        temp=[]
-        for j in range(len(frames[i])):
-            frame =frames[i][j]
-            results = detector.detect_faces(frame)
-            if len(results) > 0:
-                for face in results:
-                    x, y, width, height = face['box']
-                    face = frame[y:y+height, x:x + width]
-                    try:
-                        temp.append(face)
-                    except:
-                        pass
-        faces.append(temp)
+    results = detector.detect_faces(frame)
+    if len(results) > 0:
+        for face in results:
+            x, y, width, height = face['box']
+            face = frame[y:y+height, x:x + width]
+            faces.append(face)
     return faces
 
-def extractFeatures(faces,model):
-    features = []
-    numFaces = len(faces)
-    for i in range(len(faces)):
-        sumFaces = np.zeros(2048)
-        for j in range(len(faces[i])):
-            face = faces[i][j]
-            face = cv2.resize(face,(224,224))
-            face = face.reshape(1,224,224,3)
-            sumFaces = sumFaces + model.predict(face)
-        x = float(len(faces[i]))
-        features.append(sumFaces/x)
+def extractFeatures(face,model):
+    face = cv2.resize(face,(224,224))
+    face = face/255.0
+    face = face.reshape(1,224,224,3)
+    features =model.predict(face)
     return features
+
+def calDist(calFeatures,saveFeatures):
+    Features1 = calFeatures.reshape(2048,)
+    dist = []
+    for features in saveFeatures:
+        Features2 = features.reshape(2048,)
+        dist.append(distance.euclidean(Features1, Features2)) 
+    return min(dist)
+
+def faceVerify(model,savedFeatures,name):
+    cap = cv2.VideoCapture(0)
+    t1 = datetime.now()
+    text = '''Scaning Your Face'''
+    while cap.isOpened():
+        ret, frame = cap.read()
+        t2 = datetime.now()
+        x = t2 - t1         
+        if x.seconds==5:
+            if type(frame) != type(None):            
+                facesExt = extractFace(frame)
+                if len(facesExt)==1:
+                    featuresExt = extractFeatures(facesExt[0],model)
+                    dist = calDist(featuresExt,savedFeatures)
+                    if dist < 0.08:
+                        print("Verified \n Welcome "+name)
+                        break
+                    else:
+                        t1 = datetime.now()
+                        text = '''Trying Again'''
+                        continue
+        frame = cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.imshow('Verify Face', frame)
+        if cv2.waitKey(1) & 0XFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
 
 
 def saveFeatures(features,name):
@@ -53,96 +78,31 @@ def saveFeatures(features,name):
     featureFileW = open('../SavedFeatures/Features.pkl', 'wb')
     pk.dump(data,featureFileW)
 
-def calDist(calFeatures,preFeatures):
-    temp =[]
-    for i in range(len(calFeatures)):
-        Features1 = calFeatures[i].reshape(2048,)
-        for j in range(len(preFeatures)):
-            Features2 = preFeatures[j].reshape(2048,)
-            temp.append(distance.euclidean(Features1, Features2)) 
-    return np.min(temp)
 
 
-def newUser(model):
-    name = input("Enter your name \n")
+
+def newUser(model,name):
     cap = cv2.VideoCapture(0)
-    frames = []
     text = '''Press "a" to capture face'''
+    featuresExtAll = []
     while cap.isOpened():
         ret, frame = cap.read()         
         if cv2.waitKey(1) & 0XFF == ord('a'):
-            temp =[]
-            i = 0
-            while i <10:
-                ret, frame = cap.read()         
-                try:
-                    temp.append(frame)
-                    i = i +1
-                except:
-                    pass
-            frames.append(temp)
-            text = '''Press "a" to capture different pose '''
+            if type(frame) != type(None):            
+                facesExt = extractFace(frame)
+                if len(facesExt)==1:
+                    featuresExt = extractFeatures(facesExt[0],model)
+                    featuresExtAll.append(featuresExt)
+                    text = '''Press "a" to capture different pose '''
         frame = cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
-        cv2.imshow('Capture Face', frame)
+        cv2.imshow('Add New User', frame)
         if cv2.waitKey(1) & 0XFF == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
-    faces= extractFace(frames)
-    features = extractFeatures(faces,model)
-    ok =1
-    for f in features:
-        if np.isnan(f).any():
-            ok = 0
-    if ok == 0:
-        print("Try again \n")
-        newUser(model)
-    
-        
-    saveFeatures(features,name)
+    saveFeatures(featuresExtAll,name)
     print("Successfully Added new member \n")
 
 
-def faceRec(model,data):
-    cap = cv2.VideoCapture(0)
-    frames = []
-    while cap.isOpened():
-        ret, frame = cap.read()         
-        if cv2.waitKey(1) & 0XFF == ord('s'):
-            temp=[]    
-            temp.append(frame)   
-            frames.append(temp)
-            break    
-        frame = cv2.putText(frame, '''Press "s" to scan face''', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
-        cv2.imshow('Scan Face', frame)
-        if cv2.waitKey(1) & 0XFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    faces= extractFace(frames)
-    features = extractFeatures(faces,model)
-    temp=[]
-    for i in range(len(data)):
-        preFeatures = data[i]["features"]
-        try:
-            x = calDist(features,preFeatures)
-            temp.append(x)
-        except:
-            faceRec(model,data)
-    try:        
-        err = np.min(temp)
-    
-        if err<71:
-            user = temp.index(err)
-            user = data[user]['name']
-            print("Welcome " + user + "\n")
-            print(err)
-        else:
-            print("Sorry member not found \n")
-            print(err)
-    except:
-        pass
 
 
-        
-    
--- src/main.py
+++ src/main.py
@@ -3,20 +3,30 @@ from matplotlib import pyplot as plt
 from functions import *
 
 def main(model):
-    print("Welcome to FIGURE IT OUT ENTERPRISE")
+    print("\n \n \n Welcome to FIGURE IT OUT ENTERPRISE")
     while True:
         try:
-            com = int(input("Press the following options: \n 1. To scan face to existing members \n 2. To register as new member \n 3. To exit \n"))
+            com = int(input("Press the following options: \n 1. To verify as existing member \n 2. To register as new member \n 3. To exit \n"))
         except:
             print("Invalid Input \n")
             continue
 
         if com == 1:
             featureFileR = open('../SavedFeatures/Features.pkl', 'rb')
-            data = pk.load(featureFileR)
-            faceRec(model,data)
+            savedFeatures = pk.load(featureFileR)
+            name = input("Enter name \n")
+            flag =0
+            for user in savedFeatures:
+                if user['name'] == name:
+                    flag =1
+                    savedFeatures = user['features']
+            if flag == 1:    
+                faceVerify(model,savedFeatures,name)
+            else:
+                print("No such name exist try again \n")
         elif com == 2:
-            newUser(model)
+            name = input("Enter name \n")
+            newUser(model,name)
         elif com == 3:
             break
         else:
